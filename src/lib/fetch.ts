60d88a5af8c841ab26486de85a7508fb622d2c51
Mana Allaire <manaallaire@outlook.com>
2024-05-04 02:21:07 +0800

    feat: add check for if the response is in JSON
    Adds a check for if the response is in JSON and throws an error if it is not, avoiding an error being thrown after failing to parse the response as a JSON.

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -10,6 +10,10 @@ export async function fetchJSON<T extends object>(endpoint: string, parameters: 
   const response = await fetch(
     `https://tenor.googleapis.com/v2/${endpoint}?${parameters.toString()}`,
   );
+
+  if (!response.headers.get("content-type")?.includes("application/json"))
+    throw new Error(`${response.statusText} (${response.status.toString()})`);
+
   const responseJSON = (await response.json()) as Error | T;
 
   if ("error" in responseJSON) throw new GoogleAPIError(responseJSON.error);
