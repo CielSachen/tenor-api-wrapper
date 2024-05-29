ff42b844a003c7cfb01da531f8b2fb89e88f33ee
Mana Allaire <manaallaire@outlook.com>
2024-05-30 01:51:43 +0800

    feat: add object map of error status codes
    Adds an object map of the Google API error status codes.
    
    Replaces the types for `Status.code` and `Status.status` with the values of the status codes and a generic type extending the keys of the status codes, respectively.

--- src/errors/GoogleApiError.ts
+++ src/errors/GoogleApiError.ts
@@ -1,17 +1,20 @@
+import type { ErrorCodes } from "./errorCodes.js";
+
 /**
  * This message has the same semantics as `google.rpc.Status`. It uses HTTP status code instead of
  * gRPC status code. It has extra fields `status` and `errors` for backward compatibility with
  * {@link https://developers.google.com/api-client-library Google API Client Libraries}.
  *
  * _Converted from Protocol Buffers to TypeScript._
+ * @see {@link https://cloud.google.com/apis/design/errors#http_mapping Google Cloud APIs ErrorDocumentation}
  */
-export interface Status {
+export interface Status<TStatus extends keyof ErrorCodes = keyof ErrorCodes> {
   /** The HTTP status code that corresponds to `google.rpc.Status.code`. */
-  readonly code: number;
+  readonly code: ErrorCodes[TStatus];
   /** This corresponds to `google.rpc.Status.message`. */
   readonly message: string;
   /** This is the enum version for `google.rpc.Status.code`. */
-  readonly status?: string;
+  readonly status?: TStatus;
   /** This corresponds to `google.rpc.Status.details`. */
   readonly details?: readonly Record<string, unknown>[];
 }
@@ -22,17 +25,20 @@ export interface Status {
  * _Converted from Protocol Buffers to TypeScript._
  * @see {@link https://cloud.google.com/apis/design/errors#http_mapping Google Cloud APIs ErrorDocumentation}
  */
-export interface GoogleError {
+export interface GoogleError<TStatus extends keyof ErrorCodes = keyof ErrorCodes> {
   /**
    * The actual error payload. The nested message structure is for backward compatibility with
    * {@link https://developers.google.com/api-client-library Google API Client Libraries}. It also
    * makes the error more readable to developers.
    */
-  readonly error: Status;
+  readonly error: Status<TStatus>;
 }
 
 /** Represents a Google API error object. */
-export class GoogleApiError extends Error implements Omit<Status, "message"> {
+export class GoogleApiError<TStatus extends keyof ErrorCodes = keyof ErrorCodes>
+  extends Error
+  implements Omit<Status<TStatus>, "message">
+{
   /** The HTTP status code that corresponds to `google.rpc.Status.code`. */
   public readonly code;
   /** This is the enum version for `google.rpc.Status.code`. */
@@ -44,7 +50,7 @@ export class GoogleApiError extends Error implements Omit<Status, "message"> {
    * Constructs a new instance of a Google API error object.
    * @param error The actual error payload.
    */
-  constructor(error: Status) {
+  constructor(error: Status<TStatus>) {
     super(error.message);
 
     this.name = "GoogleApiError";
